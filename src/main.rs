b6a64a9c97a22161ddd332eda24f2702381c5a41
Scanf-s <sullung2yo@gmail.com>
2026-04-16 13:59:05 +0900

    feat: Add simple CPU usage checker
    Signed-off-by: Scanf-s <sullung2yo@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use sysinfo::{System};
+use std::{thread, time::Duration};
+
 fn main() {
-    println!("Hello, world!");
+    let mut sys = System::new_all();
+
+    // set cpu baseline
+    sys.refresh_cpu_all();
+    thread::sleep(Duration::from_millis(200));
+    sys.refresh_cpu_all();
+
+    // get each cpu usage
+    for cpu in sys.cpus() {
+        println!("{}: {}%", cpu.name(), cpu.cpu_usage());
+    }
+
+    // get total cpu usage
+    let total_usage: f32 = sys.cpus().iter().map(|cpu| cpu.cpu_usage()).sum();
+    let avg_usage: f32 = total_usage / sys.cpus().len() as f32;
+    println!("Total CPU Usages: {}%", avg_usage);
 }
